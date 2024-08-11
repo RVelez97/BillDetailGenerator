c18aa079940ab6262250a88b7f90c1b851bc3fb5
RVelez97 <ronald_velezburgos@outlook.com>
2024-08-11 15:15:22 -0500

    CSVButtonAdded

--- billdetailgenerator/bill_detail_generator/views.py
+++ billdetailgenerator/bill_detail_generator/views.py
@@ -1,18 +1,23 @@
+import csv
+from django.http import HttpResponse
 from django.shortcuts import render
 import re
 
 from .forms import XMLUploadForm
 
-
+header=['Fecha','Total Sin Impuestos','Impuestos','Total','Numero de factura','Razon Social','RUC']
+detail_of_bills=[]
 
 def index(request):
+    global detail_of_bills
+    detail_of_bills=[]
     return render(request,'index.html')
 
 
 def results(request):
     if request.method == 'POST':
         files = request.FILES.getlist('myfiles')
-        total_resume=[]
+        global detail_of_bills
         pattern = re.compile('<.*?>')
         for i in range(len(files)):
             archive= str(files[i].read(),'ISO-8859-1').split('\r\n')
@@ -42,8 +47,22 @@ def results(request):
                     razonSocial=(re.sub(pattern, '', line).lstrip()).removesuffix('\n')
                 elif('ruc' in line):
                     ruc=(re.sub(pattern, '', line).lstrip()).removesuffix('\n')
-            total_resume.insert(-1,[fechaEmision,baseImponible,valor,importeTotal,numFactura,razonSocial,ruc])
+            detail_of_bills.insert(-1,[fechaEmision,baseImponible,valor,importeTotal,numFactura,razonSocial,ruc])
         
 
         
-        return render(request,'results.html',context={'total_resume':total_resume})
+        return render(request,'results.html',context={'total_resume':detail_of_bills})
+    
+def download_as_csv(request):
+    global header
+    global detail_of_bills
+    response = HttpResponse(
+        content_type="text/csv",
+        headers={"Content-Disposition": 'attachment; filename="report.csv"'},
+    )
+
+    writer = csv.writer(response)
+    writer.writerow(header)
+    writer.writerows([detail for detail in detail_of_bills])
+
+    return response
