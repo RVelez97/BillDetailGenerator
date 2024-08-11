99eb21ed3ab8b3e56f4025c93ac5d297522aa83e
RVelez97 <ronald_velezburgos@outlook.com>
2024-08-11 14:25:27 -0500

    download as csv now available

--- billdetailgenerator/bill_detail_generator/urls.py
+++ billdetailgenerator/bill_detail_generator/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("results/", views.results, name="results"),
+    path("download_csv/",views.download_as_csv,name='download_as_csv')
 ]
